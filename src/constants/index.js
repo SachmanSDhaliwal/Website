1aa663459d99a9c83c1d3096443fafb5bddad9e5
SachmanSDhaliwal <120832709+SachmanSDhaliwal@users.noreply.github.com>
2024-03-18 11:11:39 -0400

    Add files via upload

---
+++ src/constants/index.js
@@ -0,0 +1,234 @@
+import {
+    mobile,
+    backend,
+    creator,
+    web,
+    javascript,
+    python,
+    html,
+    css,
+    reactjs,
+    redux,
+    tailwind,
+    nodejs,
+    mongodb,
+    git,
+    figma,
+    java,
+    meta,
+    gdsc,
+    tesla,
+    shopify,
+    carrent,
+    jobit,
+    tripguide,
+    threejs,
+    gsk,
+  } from "../assets";
+  
+  export const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "work",
+      title: "Work",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
+  
+  const services = [
+    {
+      title: "Web Developer",
+      icon: web,
+    },
+    {
+      title: "React Native Developer",
+      icon: mobile,
+    },
+    {
+      title: "Backend Developer",
+      icon: backend,
+    },
+    {
+      title: "Content Creator",
+      icon: creator,
+    },
+  ];
+  
+  const technologies = [
+    {
+      name: "HTML 5",
+      icon: html,
+    },
+    {
+      name: "CSS 3",
+      icon: css,
+    },
+    {
+      name: "JavaScript",
+      icon: javascript,
+    },
+    {
+      name: "React JS",
+      icon: reactjs,
+    },
+    {
+      name: "Redux Toolkit",
+      icon: redux,
+    },
+    {
+      name: "Tailwind CSS",
+      icon: tailwind,
+    },
+    {
+      name: "Node JS",
+      icon: nodejs,
+    },
+    {
+      name: "MongoDB",
+      icon: mongodb,
+    },
+    {
+      name: "Three JS",
+      icon: threejs,
+    },
+    {
+      name: "git",
+      icon: git,
+    },
+    {
+      name: "python",
+      icon: python,
+    },
+    {
+      name: "java",
+      icon: java,
+    },
+  ];
+  
+  const experiences = [
+    {
+      title: "VP of Events",
+      company_name: "GDSC(Google Student Developers Club)",
+      icon: gdsc,
+      iconBg: "#383E56",
+      date: "September 2023 - PRESENT",
+      points: [
+        "Orchestrated the planning and execution of 15+ dynamic events, such as workshops, coding competitions, and hackathons, resulting in a 30% increase in member engagement and a 20% rise in overall club attendance.",
+       "Provided visionary leadership to a team of 5 event coordinators, fostering a collaborative environment and facilitating skill development, resulting in a 25% improvement in event efficiency and a consistent average attendance of 60+ members per event.",
+      ],
+    },
+    {
+      title: "Data Analyst",
+      company_name: "GSK Transport",
+      icon: gsk,
+      iconBg: "#E6DEDD",
+      date: "May 2022 - September 2022",
+      points: [
+        "Diligently executed over 5,000+ data entry requests and tasks, consistently meeting tight deadlines with a 99.9% accuracy rate, utilizing advanced Excel spreadsheets.",
+        "Leveraged SQL queries and stored procedures, optimizing the data retrieval and reporting processes, resulting in a 30% improvement in efficiency, and ensuring real-time visibility into inventory levels and shipment tracking.",
+      ],
+    },
+
+   
+  ];
+  
+  const testimonials = [
+    {
+      testimonial:
+        "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+      name: "Sara Lee",
+      designation: "CFO",
+      company: "Acme Co",
+      image: "https://randomuser.me/api/portraits/women/4.jpg",
+    },
+    {
+      testimonial:
+        "I've never met a web developer who truly cares about their clients' success like Rick does.",
+      name: "Chris Brown",
+      designation: "COO",
+      company: "DEF Corp",
+      image: "https://randomuser.me/api/portraits/men/5.jpg",
+    },
+    {
+      testimonial:
+        "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+      name: "Lisa Wang",
+      designation: "CTO",
+      company: "456 Enterprises",
+      image: "https://randomuser.me/api/portraits/women/6.jpg",
+    },
+  ];
+  
+  const projects = [
+    {
+      name: "Image Valut",
+      description:
+        "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "mongodb",
+          color: "green-text-gradient",
+        },
+        {
+          name: "tailwind",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: carrent,
+      source_code_link: "https://github.com/",
+    },
+    {
+      name: "Job IT",
+      description:
+        "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "restapi",
+          color: "green-text-gradient",
+        },
+        {
+          name: "scss",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: jobit,
+      source_code_link: "https://github.com/",
+    },
+    {
+      name: "Trip Guide",
+      description:
+        "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+      tags: [
+        {
+          name: "nextjs",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "supabase",
+          color: "green-text-gradient",
+        },
+        {
+          name: "css",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: tripguide,
+      source_code_link: "https://github.com/",
+    },
+  ];
+  
+  export { services, technologies, experiences, testimonials, projects };
